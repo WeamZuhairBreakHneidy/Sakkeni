c90dcd5e27715d2b662617a0ad978a38296bb088
LENOVO <raghad166>
2025-08-30 15:13:04 +0300

    jjj

---
+++ lib/app/modules/payment/views/payment_view.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../controllers/payment_controller.dart';
+
+class PaymentView extends StatelessWidget {
+  const PaymentView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Get the service ID from the arguments
+    final String? serviceId = Get.arguments as String?;
+    final PaymentController controller = Get.put(PaymentController());
+
+    // You can handle the case where serviceId is null if needed
+    if (serviceId == null) {
+      return Scaffold(
+        body: Center(
+          child: Text("Error: Service ID not found."),
+        ),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Stripe Payment"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Obx listens to the isLoading variable and reacts to it
+              Obx(() {
+                if (controller.isLoading.value) {
+                  return const CircularProgressIndicator();
+                } else {
+                  return ElevatedButton(
+                    onPressed: () {
+                      // Call the makePayment function from the Controller with the serviceId
+                      controller.makePayment(serviceId: serviceId);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 40, vertical: 15),
+                      textStyle: const TextStyle(fontSize: 18),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: const Text("Pay"),
+                  );
+                }
+              }),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
