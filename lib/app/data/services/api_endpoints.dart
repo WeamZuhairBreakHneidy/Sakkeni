9dfc32c4b383bcb49ec3fb0e66607c2ea04659e4
LENOVO <raghad166>
2025-09-01 08:51:00 +0300

    jjj

--- lib/app/data/services/api_endpoints.dart
+++ lib/app/data/services/api_endpoints.dart
@@ -1,5 +1,5 @@
 class ApiEndpoints {
-  static const String baseUrl = 'http://192.168.20.74:8000';
+  static const String baseUrl = 'http://10.182.151.138:8000';
   static const String login = '/api/login';
   static const String logout = '/api/logout';
   static const String signup = '/api/sign-up';
